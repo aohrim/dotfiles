25822ad34aa7dbe180043109f49c4c7648f08e93
aohr <aohrim@proton.me>
2024-05-25 23:09:15 -0300

    feat: remove outdated vars and unused applications

--- configs/config.fish
+++ configs/config.fish
@@ -2,12 +2,12 @@ set -Ux MOZ_ENABLE_WAYLAND           1
 set -Ux QT_QPA_PLATFORM              wayland-egl
 set -Ux QT_WAYLAND_FORCE_DPI         physical
 set -Ux _JAVA_AWT_WM_NONREPARENTING  1
-set -Ux QT_QPA_PLATFORMTHEME         qt6ct
+set -Ux QT_QPA_PLATFORMTHEME         qt6ctQ
 set -Ux QT_STYLE_OVERRIDE            kvantum
 set -Ux XDG_CURRENT_DESKTOP          sway
 set -Ux VDPAU_DRIVER                 radeonsi
 set -Ux LIBVA_DRIVER_NAME            radeonsi
 set -Ua SSH_KEYS_TO_AUTOLOAD         (ls ~/.ssh/ | grep id_ | grep -v \\.)
-# env --unset=SDL_VIDEODRIVER on steam per game if necessary
-set -Ux SDL_VIDEODRIVER              wayland,x11
-set -Ux SDL_DYNAMIC_API              /usr/lib/libSDL2-2.0.so
+set -Ux QT_AUTO_SCREEN_SCALE_FACTOR  1
+SET -Ux QT_SCALE_FACTOR              1
+set -Ux QT_SCALE                     1   
